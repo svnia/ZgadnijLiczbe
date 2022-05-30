cbcbf66d28c8235522b01b0d1d43ab5b51cda350
Tomasz Piesik <tomasz.piesik@teb.edu.pl>
2022-05-30 15:48:57 +0200

    Dodaj pliki projektów.

---
+++ ZgadnijLiczbe/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random r = new Random();
+int rInt = r.Next(1, 11);
+int liczba;
+Console.WriteLine("Zgadnij liczbe:");
+while (true)
+{
+    try
+    {
+        liczba = int.Parse(Console.ReadLine());
+
+    }
+    catch (System.FormatException)
+    {
+        Console.WriteLine("Musisz podac liczbe");
+        continue;
+    }
+    if (liczba == rInt)
+    {
+        Console.WriteLine("Gratulacje Zdadłes liczbe");
+        break;
+    }
+    else if (liczba > rInt)
+    {
+        Console.WriteLine("Twoja podana liczba liczba jest mniejsza");
+    }
+    else if (liczba < rInt)
+    {
+        Console.WriteLine("Twoja podana liczba liczba jest wieksza");
+    }
+}
